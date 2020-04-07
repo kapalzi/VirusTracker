76669b689df86230e602daeab37f8e9214e20f67
krzysztof <235060@student.pwr.edu.pl>
2020-04-07 12:09:09 +0200

    Initial Commit

---
+++ VirusTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirusTracker
+//
+//  Created by Krzysztof Kapała on 07/04/2020.
+//  Copyright © 2020 Krzysztof Kapala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
